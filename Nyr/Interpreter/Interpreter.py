b7c554c6f70fcce28d8c91510945e3dfd7c91711
Niyrme <niyrme@niyrme.xyz>
2021-08-16 00:50:02 +0200

    Remove Interpreter body

--- Nyr/Interpreter/Interpreter.py
+++ Nyr/Interpreter/Interpreter.py
@@ -8,25 +8,4 @@ class Interpreter:
 	def interpret(self, node: Node.Node, env: Env) -> Any:
 		if node is None: return None
 
-		match type(node):
-			# case Node.Program:
-			# case Node.VariableDeclaration:
-			# case Node.Identifier:
-			# case Node.ExpressionStatement:
-			# case Node.EmptyStatement:
-			# case Node.BlockStatement:
-			# case Node.IfStatement:
-			# case Node.VariableStatement:
-			# case Node.WhileStatement:
-			# case Node.ForStatement:
-			# case Node.ComplexExpression:
-			# case Node.UnaryExpression:
-			# case Node.FunctionDeclaration:
-			# case Node.ReturnStatement:
-			# case Node.CallExpression:
-			# case Node.ClassDeclaration:
-			# case Node.Super:
-			# case Node.ThisExpression:
-			# case Node.NewExpression:
-			# case Node.Literal:
-			case _: raise NotImplementedError(f"{node.type} is not yet implemented.")
+		raise NotImplementedError(f"{node.type} is not yet implemented.")
