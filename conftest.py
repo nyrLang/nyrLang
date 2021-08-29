bfdd9b104f051a6e2e07f90ca7f3aa765194549a
Niyrme <niyrme@niyrme.xyz>
2021-08-29 04:24:58 +0200

    Add docstring to fixture in conftest.py

--- conftest.py
+++ conftest.py
@@ -5,7 +5,8 @@ import pytest
 
 
 @pytest.fixture
-def examplesDir():
+def examplesDir() -> Path:
+	"""Gets path to examples directory"""
 	examplesDir = Path(__file__).resolve().parent.joinpath(os.path.join(".", "examples"))
 	os.makedirs(examplesDir, exist_ok=True)
 	return examplesDir
