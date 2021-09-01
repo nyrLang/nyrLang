8540e07594f4b6371bb329182d1fbbbaae8279a1
Niyrme <niyrme@niyrme.xyz>
2021-09-01 15:02:17 +0200

    Add tests for running Parser/Interpreter multiple times on one instance

--- Nyr.Test/tests_Interpreter/test_Interpreter.py
+++ Nyr.Test/tests_Interpreter/test_Interpreter.py
@@ -7,3 +7,14 @@ def testEmptyStatement():
 	env = Interpreter().interpret(ast)
 
 	assert env == dict()
+
+
+def testInterpretMultiple():
+	parser = Parser()
+	interpreter = Interpreter()
+
+	for i in range(20):
+		ast = parser.parse(f"let x = {i};")
+		env = interpreter.interpret(ast)
+
+		assert env == {"x": i}
--- Nyr.Test/tests_Parser/test_Parser.py
+++ Nyr.Test/tests_Parser/test_Parser.py
@@ -18,3 +18,37 @@ def testParseEmpty():
 	}
 
 	assert ast == expected
+
+
+def testParseMultiple():
+	parser = Parser()
+
+	for i in range(10):
+		ast = json.loads(
+			json.dumps(
+				parser.parse(f"let x = {i};"),
+				cls=ComplexEncoder,
+			),
+		)
+
+		assert ast == {
+			"type": "Program",
+			"body": [
+				{
+					"type": "VariableStatement",
+					"declarations": [
+						{
+							"type": "VariableDeclaration",
+							"id": {
+								"type": "Identifier",
+								"name": "x",
+							},
+							"init": {
+								"type": "IntegerLiteral",
+								"value": i,
+							},
+						},
+					],
+				},
+			],
+		}
